efc97787df949b3c36e3da298e2b4b3772cde29c
Tahmasib Shirinzade <tshzade@gmail.com>
2020-11-24 13:07:57 +0400

    display categories

--- src/main.js
+++ src/main.js
@@ -8,6 +8,12 @@ Vue.config.productionTip = false
 Vue.component('AppHero', AppHero)
 Vue.component('AppDropdown', AppDropdown)
 
+Vue.filter('capitalize', function (value) {
+  if (!value) return ''
+  value = value.toString()
+  return value.charAt(0).toUpperCase() + value.slice(1)
+})
+
 new Vue({
   render: h => h(App),
 }).$mount('#app')
